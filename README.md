e4ec291671143b80c7e5caf1cfa349c8e206a63a
kelsey <kelseymcdonell@gmail.com>
2018-06-21 20:28:19 -0400

    newest

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GifTastic
+
+Very special gif generating site. Feel free to add topics and animate and pause the gifs at your pleasure.
--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,14 @@
 
    var topics = ["Cat", "Dog", "Bird", "Fish"];
    
-   $(document).on("click", ".topic-btn", displayTopicInfo);
+   $(document).on("click", ".topic-btn", searchGif);
 
    renderButtons();
 
-   function displayTopicInfo() {
-
+//    function displayTopicInfo() {
+function searchGif(topic){
     var topic = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=cYlVzTj6pzQumESU3aH6U7un3AIAEbLK&limit=1";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=cYlVzTj6pzQumESU3aH6U7un3AIAEbLK&limit=10";
 
     $.ajax({
       url: queryURL,
@@ -16,19 +16,57 @@
     }).then(function(response) {
        console.log(response);
 
-       var imageUrl1 = response.data[0].images.fixed_height_still.url;
-       var imageUrl2 = response.data[0].images.fixed_height.url;
-   
+       displayTopicInfo(response);
+    })};
 
-         var gifImage = $("<img>");
-  
-         gifImage.attr("src", imageUrl2);
-         gifImage.attr("alt", "gif image");
+//        for (i = 0; i < 9; i ++){
 
-         $("#topics-view").prepend(gifImage);
+//       var gifImage = $('<img src="response.data[i].url" data-still= "response.data[i].images.fixed_height_still.url" data-animate="response.data[i].images.fixed_height.url" data-state="still" class="gif"></img>')
+
+    
+//       $("#topics-view").prepend(gifImage);
+
+//  $(".gif").on("click", function() {
+     
+//       var state = $(this).attr("data-state");
+
+//       if (state === "still") {
+//         $(this).attr("src", $(this).attr("data-animate"));
+//         $(this).attr("data-state", "animate");
+//       } else {
+//         $(this).attr("src", $(this).attr("data-still"));
+//         $(this).attr("data-state", "still");
+//       }
+     
+      
+//     });
+//   }})}
+
+  function displayTopicInfo(response) {
+    $('#topics-view').empty();
+    for (var i = 0; i < 9; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#topics-view').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
     });
-  }
-
+}
+        
    function renderButtons() {
      $("#buttons-view").empty();
      for (var i = 0; i < topics.length; i++) {
@@ -38,7 +76,7 @@
        a.text(topics[i]);
        $("#buttons-view").append(a);
      }
-   }
+   };
 
    $("#add-topic").on("click", function(event) {
      event.preventDefault();
@@ -47,4 +85,4 @@
      topics.push(topic);    
      renderButtons();
    });
-
+  
